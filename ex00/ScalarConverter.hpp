247d6e7afbccba00ca13eea41c1539c0bd0981b9
love <tlove.tky@gmail.com>
2023-08-21 09:05:06 +0900

    ex00: argcチェック実装

--- ex00/ScalarConverter.hpp
+++ ex00/ScalarConverter.hpp
@@ -3,6 +3,12 @@
 
 #include <iostream>
 
+#define RED		"\e[0;31m" // Red
+#define GREEN	"\e[0;32m" // Green
+#define YELLOW	"\e[0;33m" // Yellow
+#define BLUE	"\e[0;34m" // Blue
+#define RESET 	"\e[0m"    // Reset
+
 class ScalarConverter
 {
 	private:
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,11 +1,16 @@
 #include "ScalarConverter.hpp"
 
-int main( void )
+int main(int argc, char *argv[])
 {
+	if (argc != 3)
+	{
+		std::cerr << RED << "Usage: " << argv[0] << " <literal>" << RESET << std::endl;
+		return 1;
+	}
 	return	0;
 }
 
 __attribute((destructor))
 static void destructor() {
-	system("leaks -q main");
+	system("leaks -q convert");
 }
