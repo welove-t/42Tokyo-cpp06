b79f42ca4d0ff337ff292c65947a303204c50b92
love <tlove.tky@gmail.com>
2023-08-22 12:54:39 +0900

    ex02: first commit

---
+++ ex02/Base.hpp
@@ -0,0 +1,22 @@
+#ifndef BASE_HPP
+#define BASE_HPP
+
+#include <iostream>
+
+#define RED		"\e[0;31m" // Red
+#define GREEN	"\e[0;32m" // Green
+#define YELLOW	"\e[0;33m" // Yellow
+#define BLUE	"\e[0;34m" // Blue
+#define RESET 	"\e[0m"    // Reset
+
+
+class Base
+{
+	private:
+	public:
+		virtual ~Base();
+
+};
+
+#endif
+
